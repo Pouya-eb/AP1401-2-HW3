b3a460a59681a5517e6790dd583872f70c55a127
Pouya-eb <Pouyaebrahimi1379@gmail.com>
2023-06-26 21:18:42 +0330

    Matrix constructor completed

--- include/matrix.h
+++ include/matrix.h
@@ -1,6 +1,29 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#include <cmath>
+#include <compare>
+#include <exception>
+#include <iomanip>
+#include <iostream>
 
+class Matrix {
+public:
+    // Constructors
+    Matrix(size_t, size_t);
+    Matrix(size_t);
+    Matrix();
+    Matrix(std::initializer_list<std::initializer_list<double>>);
+    Matrix(const Matrix&);
+    // ~Matrix() {};
+
+private:
+    // Member Variables
+    size_t _row {};
+    size_t _column {};
+    size_t _rowcap {};
+    size_t _colcap {};
+    double** matrix;
+};
 
 #endif // !MATRIX_H
--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,1 +1,48 @@
 #include "../include/matrix.h"
+
+Matrix::Matrix(size_t row, size_t column)
+    : _row { row }
+    , _column { column }
+    , _rowcap { _row }
+    , _colcap { column }
+{
+    _rowcap = _row > 2 ? pow(2, static_cast<int>(log(_row) / log(2)) + 1) : 2;
+    _colcap = _column > 2 ? pow(2, static_cast<int>(log(_column) / log(2)) + 1) : 2;
+    matrix = new double*[_rowcap];
+    for (size_t i {}; i < _rowcap; i++)
+        matrix[i] = new double[_colcap];
+}
+Matrix::Matrix(size_t size)
+    : Matrix(size, size)
+{
+}
+Matrix::Matrix()
+    : Matrix(0)
+{
+}
+Matrix::Matrix(std::initializer_list<std::initializer_list<double>> a)
+    : Matrix(a.size(), a.begin()->size())
+{
+    for (size_t i {}; i < _row; i++)
+        for (size_t j {}; j < _column; j++)
+            matrix[i][j] = static_cast<double>(*((a.begin() + i)->begin() + j));
+}
+Matrix::Matrix(const Matrix& m1)
+    : _row { m1._row }
+    , _column { m1._column }
+    , _rowcap { m1._rowcap }
+    , _colcap { m1._colcap }
+    , matrix { new double*[_row] }
+{
+    for (size_t i {}; i < _rowcap; i++)
+        matrix[i] = new double[_colcap];
+    for (size_t i {}; i < _row; i++)
+        for (size_t j {}; j < _column; j++)
+            matrix[i][j] = m1.matrix[i][j];
+}
+// Matrix::~Matrix()
+// {
+//     for (size_t i {}; i < _row; i++)
+//         delete[] matrix[i];
+//     delete[] matrix;
+// }
